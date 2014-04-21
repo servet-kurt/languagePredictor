669bb4cf4edf0b9011493fd87635dbe74f06548b
ilgün ilgün <ilgun.ilgun@gmail.com>
2014-04-22 00:42:46 +0300

    finding english words trigram completed.

--- DataMining/src/English/EnglishTrigramWords.java
+++ DataMining/src/English/EnglishTrigramWords.java
@@ -1,24 +1,56 @@
 package English;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class EnglishTrigramWords {
 
-	List<String> trigrams = Arrays.asList("THE", "AND", "ING", "ENT", "ION",
-			"HER", "FOR", "THA", "NTH", "INT", "TER", "EST", "ERS", "ATI",
-			"HAT", "ATE", "ALL", "ETH", "HIS", "OFT", "ITH", "FTH", "STH",
-			"OTH", "RES", "ONT", "ERE", "TIO", "HES", "VER");
+	public double trigramCounter(String word) {
 
-	public int isInTrigram(String word) {
-
-		for (String trigram : trigrams) {
+		for (String trigram : trigrams.keySet()) {
 			if (word.toUpperCase().indexOf(trigram) != -1) {
-				return StringUtils.countMatches(word.toUpperCase(), trigram);
+				return StringUtils.countMatches(word.toUpperCase(), trigram)*trigrams.get(trigram);
 			}
 		}
 		return 0;
 	}
+	@SuppressWarnings("serial")
+	Map<String,Double> trigrams = new HashMap<String, Double>()
+	{
+	    {
+	        put("THE", 1.81);
+	        put("AND", 0.73);
+	        put("ING", 0.72);
+	        put("ENT", 0.42);
+	        put("ION", 0.42);
+	        put("HER", 0.36);
+	        put("FOR", 0.34);
+	        put("THA", 0.33);
+	        put("NTH", 0.33);
+	        put("INT", 0.32);
+	        put("TER", 0.30);
+	        put("EST", 0.28);
+	        put("ERS", 0.28);
+	        put("ATI", 0.26);
+	        put("HAT", 0.26);
+	        put("ATE", 0.25);
+	        put("ETH", 0.24);
+	        put("ALL", 0.25);
+	        put("HIS", 0.24);
+	        put("OFT", 0.22);
+	        put("ITH", 0.21);
+	        put("FTH", 0.21);
+	        put("STH", 0.21);
+	        put("OTH", 0.21);
+	        put("RES", 0.21);
+	        put("ONT", 0.20);
+	        put("ERE", 0.31);
+	        put("TIO", 0.31);
+	        put("HES", 0.24);
+	        put("VER", 0.24);
+	    }
+	};
+
 }
--- DataMining/src/Main/Trigram.java
+++ DataMining/src/Main/Trigram.java
@@ -4,12 +4,15 @@ import English.EnglishTrigramWords;
 
 public class Trigram {
 		
+	private double englishTrigramCount;
+
 	public void FindTrigramAssociations(String input){
 		EnglishTrigramWords englishTrigramWords = new EnglishTrigramWords(); 
 		String[] words = input.split(" ");
-		 for(String word:words){
-			 System.out.println(englishTrigramWords.isInTrigram(word));
+		for(String word:words){
+			englishTrigramCount +=englishTrigramWords.trigramCounter(word);
+			
 		 }
-		 
+		 System.out.println(englishTrigramCount);
 	}
 }
